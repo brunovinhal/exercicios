ff0dc44bf6ae203acbfa111f6e2ab5909f6d0af9
vinhal4723 <vinhal4723@gmail.com>
2022-07-04 15:40:23 -0300

    Commit taboada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String valor = JOptionPane.showInputDialog("Imprima valor");
+        String valor = JOptionPane.showInputDialog("Digite valor");
         int valorInteiro = Integer.parseInt(valor);
         System.out.println(valorInteiro);
     }
