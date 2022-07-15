86a697c6d25e210063184d1c28368848f6fd915d
BRUNO VINHAL <107578297+vinhal4723@users.noreply.github.com>
2022-07-15 17:31:10 -0300

    Exercicio imprimir numeros de 150 a 300

---
+++ src/main/java/ImprimirNumeros.java
@@ -0,0 +1,10 @@
+public class ImprimirNumeros {
+
+    public static void main(String[] args) {
+        for (int imp = 150; imp <= 300; imp++) {
+            System.out.println(imp);
+        }
+    }
+}
+
+
