e9af0a297b5f6390946161e1d6f4d7c6d1ce6a9f
BRUNO VINHAL <107578297+vinhal4723@users.noreply.github.com>
2022-07-16 15:08:08 -0300

    Programa para inserir uma senha

---
+++ src/main/java/InserirSenha.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class InserirSenha {
+
+    public static void main(String[] args) {
+        int senha = 1234;
+
+        String senhaInfo = JOptionPane.showInputDialog("Digite a Senha");
+        int senhaPessoal = Integer.parseInt(senhaInfo);
+        if (senhaPessoal == senha) {
+            System.out.println("ACESSO PERMITIDO ");
+        } else {
+            System.out.println("ACESSO NEGADO");
+        }
+    }
+
+}
