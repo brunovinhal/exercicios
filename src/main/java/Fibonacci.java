8b1e407e196dffff5cf5288ec3be00909263a0b6
BRUNO VINHAL <107578297+vinhal4723@users.noreply.github.com>
2022-07-18 13:33:51 -0300

    Exercicio: desafio Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,12 +1,12 @@
 public class Fibonacci {
 
-    public	static	void	main(String[]	args) {
-        int	anterior	=	0;
-        int	atual	=	1;
-        while	(atual	<	1000)	{
+    public static void main(String[] args) {
+        int anterior = 0;
+        int atual = 1;
+        while (atual < 1000) {
             System.out.println(atual);
-            atual	=	anterior	+	atual;
-            anterior	=	atual	-	anterior;
+            atual = anterior + atual;
+            anterior = atual - anterior;
         }
         System.out.println(atual);
     }
